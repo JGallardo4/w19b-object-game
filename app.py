b2da0a50a389aeead5e9b4ed17a2e8fdc556e50b
Juan Gallardo <juan.gallardo.ab@gmail.com>
2021-02-06 12:16:50 -0700

    Implement player movement

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import gameboard
-import player
+from gameboard import GameBoard
+from player import Player
+from utils import PlayerMover
 
 print("Welcome to the game!")
 print("Instructions: ")
@@ -11,13 +12,15 @@ print("To move right: d")
 print("Try to get to the end! Good Luck!")
 print("-----------------------------")
 
-# TODO
-# Create a new GameBoard called board
-# Create a new Player called played starting at position 3,2
+board = GameBoard()
+player = Player(3, 2)
+playerMover = PlayerMover(player, board)
 
 while True:
     board.printBoard(player.rowPosition, player.columnPosition)
+    print(player.rowPosition, player.columnPosition)
     selection = input("Make a move: ")
-    # TODO
-    # Move the player through the board
+
+    playerMover.movePlayer(selection)
+    
     # Check if the player has won, if so print a message and break the loop!
